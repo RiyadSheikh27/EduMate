8b6e66fd1fe669544154d3583253113e51960fae
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-04-11 12:03:07 +0600

    Completed the status section for homework(Either checked or unchecked)

--- dashboard/views.py
+++ dashboard/views.py
@@ -21,7 +21,7 @@ def notes(request):
             notes = Notes(
                 user=request.user,
                 title=request.POST["title"],
-                description=request.POST["description"]
+                description=request.POST["description"],
             )
             notes.save()
         messages.success(
@@ -47,27 +47,27 @@ class NotesDetailView(generic.DetailView):
 
 # Creating views for home and notes
 def homework(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = HomeworkForm(request.POST)
         if form.is_valid():
             try:
-                finished = request.POST['is_finished']
-                if finished == 'on':
+                finished = request.POST["is_finished"]
+                if finished == "on":
                     finished = True
                 else:
                     finished = False
             except:
                 finished = False
             homeworks = Homework(
-                user = request.user,
-                subject = request.POST['subject'],
-                title = request.POST['title'],
-                description = request.POST['description'],
-                due = request.POST['due'],
-                is_finished = finished
+                user=request.user,
+                subject=request.POST["subject"],
+                title=request.POST["title"],
+                description=request.POST["description"],
+                due=request.POST["due"],
+                is_finished=finished,
             )
             homeworks.save()
-            messages.success(request,f'Homework Added from {request.user.username}!!')
+            messages.success(request, f"Homework Added from {request.user.username}!!")
     else:
         form = HomeworkForm()
     homework = Homework.objects.filter(user=request.user)
@@ -77,3 +77,14 @@ def homework(request):
         homework_done = False
     context = {"homeworks": homework, "homeworks_done": homework_done, "form": form}
     return render(request, "dashboard/homework.html", context)
+
+
+# Updating Homework through checkbox
+def update_homework(request, pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect("homework")
